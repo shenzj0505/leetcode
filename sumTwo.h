052e83ffefea5f4b51f355e6548edceae8137c00
shenzj0505 <44492976+shenzj0505@users.noreply.github.com>
2019-09-18 17:56:20 +1000

    Delete sumTwo.h

--- sumTwo.h
+++
@@ -1,6 +0,0 @@
-typedef struct HashMap_Class * HashMapPointer;
-HashMapPointer HashMap_Init(int initialCapacity);
-void HashMap_Put(HashMapPointer map,int key,int value);
-bool HashMap_Contains(HashMapPointer map, int key);
-void HashMap_Free(HashMapPointer map);
-int HashMap_GetValue(HashMapPointer map,int key);
